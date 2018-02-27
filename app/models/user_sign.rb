b62415f2fae8fe2c2926da406b98caed30383c6b
Hiveer <ljp.hiveer@gmail.com>
2018-02-27 10:52:09 +0800

    here, the recreated as_json method should compatible with existing method

--- app/models/user_sign.rb
+++ app/models/user_sign.rb
@@ -2,7 +2,7 @@ class UserSign < ApplicationRecord
   validates_presence_of :os_name, :role_id, :role_name, :sign
   validates_uniqueness_of :role_name
 
-  def as_json
+  def as_json options = nil
     super.slice("os_name", "role_id", "role_name", "sign")
   end
 end
