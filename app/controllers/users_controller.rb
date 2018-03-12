81d1b852ac0b8161676877ef812504194adb10f8
Hiveer <ljp.hiveer@gmail.com>
2018-03-12 17:12:08 +0800

    set the list sort by 'top_kill' by default

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def index
-    @users = UsersFromNetease.users.map{|user|user['result']}
+    @users = UsersFromNetease.users.map{|user|user['result']}.sort_by {|user| user['top_kill']}.reverse
   end
 end
