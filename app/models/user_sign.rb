a837a255d8e427ad55d685eaa53683d1269e3a06
Hiveer <ljp.hiveer@gmail.com>
2018-03-16 10:40:16 +0800

    add validation to user_signs

--- app/models/user_sign.rb
+++ app/models/user_sign.rb
@@ -1,6 +1,16 @@
 class UserSign < ApplicationRecord
-  validates_presence_of :os_name, :role_id, :role_name, :sign
-  validates_uniqueness_of :role_name
+  validates :os_name, inclusion: { in: ['ios', 'android'] }
+  # validates_length_of :role_id, is: 16, message: "invalid role id"
+  # validates_length_of :sign, in: 116..124, message: "invalid sign"
+
+  # NOTE: when using validates, we can't customize the error message
+  # validates :role_id, length: { is: 16 }
+  # validates :sign, length: { in: 116..124 }
+
+  validate do
+    errors.add(:role_id, 'invalid role id') unless role_id.length == 16
+    errors.add(:sign, 'invalid sign') unless sign.length.between?(116,124)
+  end
 
   def as_json options = nil
     super.slice("os_name", "role_id", "role_name", "sign")
